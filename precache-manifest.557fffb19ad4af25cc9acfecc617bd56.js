0cb27131d9e1ec981b40af5f5dccfb65959a5660
Abhishek Upperwal <a.upperwal@gmail.com>
2020-07-26 16:39:50 +0530

    Updates

---
+++ precache-manifest.557fffb19ad4af25cc9acfecc617bd56.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a4a40b3c1037d011dbf15094c44495",
+    "url": "/data-challenge/index.html"
+  },
+  {
+    "revision": "65d3b303373e69836c43",
+    "url": "/data-challenge/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cbd2b5bdfba2dc491611",
+    "url": "/data-challenge/static/css/main.601c9a12.chunk.css"
+  },
+  {
+    "revision": "65d3b303373e69836c43",
+    "url": "/data-challenge/static/js/2.5d627a01.chunk.js"
+  },
+  {
+    "revision": "ffb699516f032882645f06689e38c1df",
+    "url": "/data-challenge/static/js/2.5d627a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd2b5bdfba2dc491611",
+    "url": "/data-challenge/static/js/main.926818dd.chunk.js"
+  },
+  {
+    "revision": "292702babd3b71a9f409",
+    "url": "/data-challenge/static/js/runtime-main.8717b22c.js"
+  },
+  {
+    "revision": "9a28c02580fe3df9930e08b10dba803f",
+    "url": "/data-challenge/static/media/BwNistaInt-xBd.9a28c025.woff"
+  },
+  {
+    "revision": "53ce8c8840322a10f92c5828d3221e22",
+    "url": "/data-challenge/static/media/Consolas.53ce8c88.ttf"
+  },
+  {
+    "revision": "a7d27908bf61e9052f2aa3878a77840e",
+    "url": "/data-challenge/static/media/air-pollution.a7d27908.svg"
+  },
+  {
+    "revision": "6e53849c5497e003dc241cae5e8875e7",
+    "url": "/data-challenge/static/media/area.6e53849c.svg"
+  },
+  {
+    "revision": "0d5feb17ddf0bb8dadc6bc9fa6f2e173",
+    "url": "/data-challenge/static/media/bus.0d5feb17.svg"
+  },
+  {
+    "revision": "d57048d1c154303d17dc71df808c5546",
+    "url": "/data-challenge/static/media/distance.d57048d1.svg"
+  },
+  {
+    "revision": "ef0b04ba847ac26a86cb55ef90391b04",
+    "url": "/data-challenge/static/media/dust.ef0b04ba.svg"
+  },
+  {
+    "revision": "9ab2f4671e0f052b2966190cb511c9d9",
+    "url": "/data-challenge/static/media/pm10.9ab2f467.svg"
+  },
+  {
+    "revision": "af9101c801a751cf3771195fe5b7794d",
+    "url": "/data-challenge/static/media/pollution.af9101c8.svg"
+  },
+  {
+    "revision": "9ae381caf0af633b3df054c7e2c7365a",
+    "url": "/data-challenge/static/media/population.9ae381ca.svg"
+  },
+  {
+    "revision": "9c0f1fe282e106c87d17cf9b58d575a2",
+    "url": "/data-challenge/static/media/ridership.9c0f1fe2.svg"
+  },
+  {
+    "revision": "a367c6c382bc2f076df0c733daae07dc",
+    "url": "/data-challenge/static/media/seats.a367c6c3.svg"
+  }
+]);
